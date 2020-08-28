754f857a177a8d2af2fc6a9f0f6d496a225eadd7
NailMukhametshin <nail.zubr@gmail.com>
2020-08-28 23:26:43 +0300

    Revert "fixed InvocationTargetException"
    This reverts commit d1cabc23

--- src/main/java/ru/technaxis/library/LibraryApplication.java
+++ src/main/java/ru/technaxis/library/LibraryApplication.java
@@ -11,13 +11,13 @@ import java.util.List;
 public class LibraryApplication {
 
     public static void main(String[] args) {
-        var context = SpringApplication.run(LibraryApplication.class);
+        var context = SpringApplication.run(LibraryApplication.class, args);
         var bookRepository = context.getBean(BookRepository.class);
 
         bookRepository.saveAll(List.of(
                 new BookEntity(
                         1,
-                        "\"Бойцовский клуб\"",
+                        "'Бойцовский клуб'",
                         "Интриги. Хаос. Мыло.",
                         "Чак Паланик",
                         "978-5-17-016682-4",
@@ -26,7 +26,7 @@ public class LibraryApplication {
                         "FightClub.jpg"),
                 new BookEntity(
                         2,
-                        "\"Над пропастью во ржи\"",
+                        "'Над пропастью во ржи'",
                         "Герой повести современного американского прозаика подросток, только вступающий в жизнь, не хочет подчиняться ханжеским законам мира взрослых. С юношеской непримиримостью он восстает против них и сам остро переживает создаваемые конфликты.",
                         "Джером Сэлинджер",
                         "978-5-699-30534-6",
@@ -35,13 +35,14 @@ public class LibraryApplication {
                         "TheCatcherInTheRye.jpg"),
                 new BookEntity(
                         3,
-                        "\"Чапаев и Пустота\"",
+                        "'Чапаев и Пустота'",
                         "Действие книги происходит в 1919 году в дивизии Чапаева, в которой главный герой, поэт-декадент Петр Пустота, служит комиссаром, а также в наши дни, а также, как и всегда у Пелевина, в виртуальном пространстве",
                         "Виктор Пелевин",
                         "5-9560-0083-Х",
                         2003,
                         false,
                         "Chapaev.jpg")
-        ));
+                ));
     }
+
 }
